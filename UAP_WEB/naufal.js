a80f0f6d0f582fabba8cb8aaaef5ee50e39a1425
pepehboy <127196085+pepehboy@users.noreply.github.com>
2023-12-28 19:56:59 +0700

    Add files via upload

---
+++ UAP_WEB/naufal.js
@@ -0,0 +1,35 @@
+//data barang
+do {
+    lert
+} while (barang); barang [
+{
+    Kode : "001",
+    Nama : "A",
+    Harga : 10000
+},
+{
+    Kode : "003",
+    Nama : "B",
+    Harga : 20000
+},
+{
+    Kode : "003",
+    Nama : "C",
+    Harga : 30000
+
+}
+],
+{
+function :( "inputan user"),
+inputan : ("masukan code"),
+inputan :("banyaknya barang") ,
+
+import: <AbortController inputan user>
+try {
+    
+} catch (error) {
+    true
+}
+</AbortController>
+
+}
